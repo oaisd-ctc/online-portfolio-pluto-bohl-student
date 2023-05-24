f6a59b4453da29a7cb01f5c3eedd50c0459bb8bd
Pluto <112419543+pluto-bohl-student@users.noreply.github.com>
2023-05-24 18:43:25 +0000

    what

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,5 +1,42 @@
 "use strict";
 
-function whar() {
-  return;
+console.log("Starting Asteroid1 Animation...");
+var milliseconds = 0,
+    seconds = 0,
+    minutes = 0,
+    hours = 0;
+var _int = null;
+_int = setInterval(displayTimer, 10);
+var id = setInterval(frame, 5);
+var done = false;
+
+function frame() {
+  var asteroid = document.getElementById("asteroid1");
+  var pos = 0;
+
+  if (done == true) {
+    clearInterval(id);
+  } else {// console.log("test") Test worked
+  }
 }
+
+function displayTimer() {
+  milliseconds += 10;
+
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+
+      if (minutes == 60) {
+        minutes == 0;
+        hours++;
+      }
+    }
+  }
+}
+
+console.log("Asteroid Animation Complete!");
--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-function whar() {
-    return
+console.log("Starting Asteroid1 Animation...");
+
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let int = null;
+
+int = setInterval(displayTimer,10);
+
+var id = setInterval(frame, 5);
+var done = false;
+
+function frame() {
+    var asteroid = document.getElementById("asteroid1");
+    var pos = 0;
+
+    if (done == true) {
+        clearInterval(id);
+    } else {
+        // console.log("test") Test worked
+    }
 }
+
+function displayTimer() {
+    milliseconds+=10;
+    if (milliseconds == 1000) {
+        milliseconds = 0;
+        seconds ++;
+        if (seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes == 60) {
+                minutes == 0;
+                hours++;
+            }
+        }
+    }
+}
+
+console.log("Asteroid Animation Complete!");
