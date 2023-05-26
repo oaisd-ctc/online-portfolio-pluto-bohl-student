c03b22beda8cdd1c86fb4c081b750dd9e0512b19
Pluto <112419543+pluto-bohl-student@users.noreply.github.com>
2023-05-26 18:40:45 +0000

    asasdasdasdasdasdasdads

--- js/main.js
+++ js/main.js
@@ -1,37 +1,10 @@
-const anime = require('animejs');
+// import anime from '../node_modules/animejs/lib/anime.js';
 
-let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
-let int = null;
-
-let totalTime = null;
-
-var id = null;
-
-function timer() {
-    milliseconds+=10;
-    if (milliseconds == 1000) {
-        milliseconds = 0;
-        seconds ++;
-        if (seconds == 60) {
-            seconds = 0;
-            minutes++;
-        }
-    }
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
-
-    let totalTime = s + "." + ms;
-
-    return totalTime;
-}
-
-var animTest = anime({
-    targets: 'div',
-    translateX: 100,
-    borderRadius: 50,
-    duration: 2000,
-    easing: 'linear',
-    direction: 'alternate'
-})
-
-document.querySelector('.play-pause-demo .play').onclick = animation.play;
+// anime({
+//     targets: 'div',
+//     left: '240px',
+//     backgroundColor: '#FFF',
+//     borderRadius: ['0%', '50%'],
+//     easing: 'easeInOutQuad',
+//     autoplay: true
+// });
